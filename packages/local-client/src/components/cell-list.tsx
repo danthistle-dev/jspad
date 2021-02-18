4e4159473658e0ba53f79dad867cc8cda4912778
danthistle-dev <contact@danthistlethwaite.com>
2021-02-18 11:55:42 +1000

    Initial commit

---
+++ packages/local-client/src/components/cell-list.tsx
@@ -0,0 +1,31 @@
+import './cell-list.css';
+import { Fragment, useEffect } from 'react';
+import { useTypedSelector } from '../hooks/use-typed-selector';
+import { useActions } from '../hooks/use-actions';
+import CellListItem from './cell-list-item';
+import AddCell from './add-cell';
+
+const CellList: React.FC = () => {
+  const cells = useTypedSelector(({ cells: { order, data } }) => order.map((id) => data[id]));
+  const { fetchCells } = useActions();
+
+  useEffect(() => {
+    fetchCells();
+  }, []);
+
+  const renderedCells = cells.map((cell) => (
+    <Fragment key={cell.id}>
+      <CellListItem cell={cell} />
+      <AddCell prevCellId={cell.id} />
+    </Fragment>
+  ));
+
+  return (
+    <div className="cell-list">
+      <AddCell forceVisible={cells.length === 0} prevCellId={null} />
+      {renderedCells}
+    </div>
+  );
+};
+
+export default CellList;
